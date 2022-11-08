ec761e6cd68dea681518b726ba968153e36f5ad6
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2022-11-08 22:43:16 +0500

    updated acsses control

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const cors = require('cors')
 
 const app = express()
-app.use(cors( ))
+app.use(cors('*'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'./uploads')))
 
